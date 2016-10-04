603ed041482d528b6fda3ecf6e1adbd3fc70e29e
Vaxl <vaxl@mail.ru>
2016-10-04 17:51:51 +0300

    ok

--- src/main/java/ru/vaxl/Client/Main.java
+++ src/main/java/ru/vaxl/Client/Main.java
@@ -4,7 +4,7 @@ package ru.vaxl.Client;
 
 public class Main {
     public static void main(String[] args) {
-        final String SERVER_IP = "localhost";
+        final String SERVER_IP = args[0];
 
         try (Client client = new Client(SERVER_IP)){
             client.start();
--- src/main/java/ru/vaxl/Server/Main.java
+++ src/main/java/ru/vaxl/Server/Main.java
@@ -4,9 +4,9 @@ package ru.vaxl.Server;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        final String SERVER_IP = "localhost";
-        final String DIRECTORY = "C:\\temp";
-        final int SCAN_RATE = Integer.valueOf("50");
+        final String SERVER_IP = args[0];
+        final String DIRECTORY = args[1];
+        final int SCAN_RATE = Integer.valueOf(args[2]);
 
         try (Server server = new Server(SERVER_IP,DIRECTORY,SCAN_RATE)){
             server.start();
